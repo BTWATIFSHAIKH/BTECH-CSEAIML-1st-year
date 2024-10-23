ff78c209a9af4fcc9a8afa47c3ea257cfa6006c9
Atif <shaikhatif5551975@gmail.com>
2024-10-23 17:05:51 +0530

    Add files via upload

---
+++ AATIF.py
@@ -0,0 +1,107 @@
+# ARITHMETIC OPERATOR
+# print("arithmetic operator")
+# a=10
+# b=5
+# print("addition:",a+b)
+# print("substraction:",a-b)
+# print("multiplication:",a*b)
+# print("division:",a/b)
+# print("floor division:",a//b) 
+# print("modulus:",a%b)
+# print("exponent",a**b)
+
+#REGIONAL OPERATOR
+# x=15
+# y=30
+# print('x > y is ',x>y)
+# print('x < y is ',x<y)
+# print('x == y is ',x==y)
+# print('x != y is ',x!=y)
+# print('x >= y is ',x>=y)
+# print('x <= y is ',x<=y)
+
+#LOGICAL OPERATOR 
+# print("logical operator")
+# print(10<5 and 10<20)
+# print(10<5 or 10<20)
+# print(not(10<20))
+
+#  temperature=int(input("enter temperature"))
+#  if temperature<40:
+#        print("cold")
+#  else:
+#        print("Nice")
+
+
+# base_hours=40   # Base hours per week 
+# ot_multiplier=1.5  # Overtime multiplier
+
+
+# hours=int(input('enter the number of hours worked:'))
+# pay_rate=int(input('Enter the hourly pay rate:'))
+
+# if hours > base_hours:
+#     overtime_hours=hours-base_hours
+#     overtime_pay=overtime_hours*pay_rate*ot_multiplier
+#     gross_pay=base_hours*pay_rate+overtime_pay
+# else:
+#     gross_pay=hours*pay_rate
+# print('the gross pay is $',format(gross_pay,',.2f'),sep='')
+
+# x=25
+# if x<10 :
+#     print('smaller')
+# if x>20 :
+#     print('bigger')
+
+# print('Finish')
+
+# x=5
+# if x==5 :
+#     print('Equals 5')
+# if x>4  :
+#     print('Greater than 4')
+# if x>=5 :
+#     print('greater than or Equal 5')
+# if x<6 : print('less than 6')
+# if x<=5 :
+#     print('less than or equals 5 ')
+# if x!=6  :
+#     print('Not Equal 6')    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
