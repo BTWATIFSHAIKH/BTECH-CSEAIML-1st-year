e4329c0ef70e4afc37172327934124abc9ac277c
Atif <shaikhatif5551975@gmail.com>
2024-10-25 11:33:07 +0530

    Add files via upload

---
+++ AATIF 3.py
@@ -0,0 +1,53 @@
+# Write a python code to find the last digit of a number
+#Ex    
+   #   Input:123456789
+   #   Output:9
+
+   #   Input:3572864824
+   #   Output:4
+
+
+# number=int(input("Enter Some Number"))
+# last_digit=number%10
+# print(last_digit)
+
+
+#Check wheather a number is even or odd. 
+# number=int(input("Enter Some Number"))
+# if number%2==0:
+#     print("Number is Even")
+# else:
+#     print("Number is odd")
+
+#Check wheather a number is divisible by both 3 and 5.
+# number=int(input("Enter a number"))
+# if number%3==0 and number%5==0:
+#     print("Number is divisible 3 and 5")
+# if number%3==0 and number%5!=0:
+#     print("Number is Divisible by 3 but not 5")
+# if number%5==0 and number%3!=0:
+#      print("Number is Divisible by 5 but not 3")
+# if number%3!=0 and number%5!=0:
+#      print("Number is neither divisible by 3 nor 5")      
+#     
+
+
+# Create a python program that simulates a traffic light system . The user inputs one of the following colors:    
+#  "red", "yellow", or "green" . Based on the input, the program should print: 
+# "stop" for red ,
+# "get ready" for yellow
+# "Go" for green
+
+colour=(input("Enter a colour"))
+if colour=="R":  #R=RED
+    print("STOP")
+if colour=="Y":  #Y=YELLOW
+    print("GET READY")
+if colour=="G":  #G=GREEN
+    print("GO")      
+
+
+
+
+
+
