7bb18e1ae72d9947c1fefd78aa4c0251f0ed6bb0
Atif <shaikhatif5551975@gmail.com>
2024-11-22 09:38:45 +0530

    Add files via upload

---
+++ AAtif4.py
@@ -0,0 +1,37 @@
+# age=int(input("Enter a Number:"))
+# if age < 18: 
+#   print("Teenager")
+# elif age >= 18 and age <= 30:
+#     print("Adult")
+# else :
+#     print("senior")
+    
+person1= int(input("Enter a Number"))
+person2= int(input("Enter a Number"))
+person3= int(input("Enter a Number"))
+if person1 < 18 :
+    print(" person1 is Teenager")
+if person1 >= 18 and person2 <= 30 :
+    print("person1 is Adult")
+if person1 > 30 :
+    print("person1 is Senior")
+
+if person2 < 18 :
+    print("person2 is Teenager")
+if person2 >= 18 and person2 <= 30 :
+    print("person2 is Adult")
+if person2 > 30 :
+    print("person2 is Senior")    
+            
+if person3 < 18 :
+    print("person3 is Teenager")
+if person3 >= 18 and person2 <= 30 :
+    print("person3 is Adult")
+if person3 > 30 :
+    print("person3 is Senior")    
+
+        
+     
+  
+
+
